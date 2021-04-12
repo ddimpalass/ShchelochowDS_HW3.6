078f70bf7aa49412002e911ad892922fcc591529
Дмитрий Щелочков <ddimpalass@icloud.com>
2021-04-12 09:35:51 +0300

    Initial Commit

---
+++ ShchelochowDS_HW3.6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShchelochowDS_HW3.6
+//
+//  Created by Apple on 12.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShchelochowDS_HW3.6/ShchelochowDS_HW3_6App.swift
@@ -0,0 +1,17 @@
+//
+//  ShchelochowDS_HW3_6App.swift
+//  ShchelochowDS_HW3.6
+//
+//  Created by Apple on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShchelochowDS_HW3_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
